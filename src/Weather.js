d1453351e5ef7469cbfb3e03a91ad3ab4876d246
jcedwards48 <105121272+jcedwards48@users.noreply.github.com>
2022-10-17 10:30:17 -0400

    new file
    added new .css file

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React from "react";
+import "./"Weather.css";
 
 export default function Weather() {
   return;
-  <div>Hello from Weather</div>;
+  <div classname="Weather">Hello from Weather</div>;
 }
