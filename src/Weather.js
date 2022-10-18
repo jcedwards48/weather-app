bd8f5a8963d4993dfc295793d30fe676100867a4
jcedwards48 <105121272+jcedwards48@users.noreply.github.com>
2022-10-18 09:59:57 -0400

    Modify
    changed background color

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,12 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div classname="Weather">Hello from Weather</div>;
+  return (
+  <div classname="Weather">Blythewood</div>;
+<ul>
+  <li> 15 October 2022  10:00 </li>
+  <li> Sunny </li>
+</ul>
+
+  );
 }
