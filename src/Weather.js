7a01a5402fef86223fb07301b1d99d61715fba6c
jcedwards48 <105121272+jcedwards48@users.noreply.github.com>
2022-10-19 13:35:19 -0400

    Update Weather.js
    corrected syntax error

--- src/Weather.js
+++ src/Weather.js
@@ -44,7 +44,7 @@ export default function Weather() {
             className="float-left"
           />
           <div className="float-left">
-            <span className="temperature">{Math.round.(temperature)}</span>
+            <span className="temperature">{Math.round(temperature)}</span>
             <span className="unit">°C</span>
           </div>
         </div>
